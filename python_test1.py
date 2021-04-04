5cfcfb2ee2ce6443d4c9e473af29a084d4e0b65a
jonnathan <jonnathantgil97@gmail.com>
2021-04-04 16:03:24 -0500

    added variable y_2

--- python_test1.py
+++ python_test1.py
@@ -9,8 +9,14 @@ import numpy as np
 x = np.array(list(range(0, 10, 1)))
 
 print(x)
-y = x**2
 
-plt.plot(x,y, color='red')
+y = x**2
+print(y)
+
+
+y_2 = 3*x-2
+
+plt.plot(x,y, color='blue')
+plt.plot(x,y_2, color ='yellow', ls=':')
 
 plt.show()
