49d642187fdb4d0326742fe46f9a56496e92fe0b
Camilo Andres Borda Gil <caabordagi@unal.edu.co>
2021-04-04 16:33:45 -0500

    no je ke ize

--- python_test1.py
+++ python_test1.py
@@ -3,14 +3,4 @@ print('Desde jonnathan')
 print('Camilo es gay')
 print('monogay')
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.array(list(range(0, 10, 1)))
-
-print(x)
-y = x**2
-
-plt.plot(x,y, color='red')
-
-plt.show()
+print('testing changes')
