938f7df97f7906b18aa04846db45f722dca39800
Camilo Andres Borda Gil <caabordagi@unal.edu.co>
2021-04-04 15:42:27 -0500

    Added plot

--- python_test1.py
+++ python_test1.py
@@ -2,3 +2,15 @@ print('hola mundo')
 print('Desde jonnathan')
 print('Camilo es gay')
 print('monogay')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(list(range(0, 10, 1)))
+
+print(x)
+y = x**2
+
+plt.plot(x,y, color='red')
+
+plt.show()
